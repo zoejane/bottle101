eb10b9c2d4b9543b38c102407d5f1f7d61b6e899
Zoe <dadac123@gmail.com>
2015-11-08 09:51:04 +0800

    add login

--- hello-simple.py
+++ hello-simple.py
@@ -9,4 +9,23 @@ def hello():
 def greet(name='Stranger'):
     return template('Hello {{name}}, how are you?', name=name)
 
+@route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
 run(host='localhost', port=8080, debug=True)

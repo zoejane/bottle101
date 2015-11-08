b95b93ad9ff629a970bbfe5ec1ead73c7da3daf7
Zoe <dadac123@gmail.com>
2015-11-08 12:01:36 +0800

    add diary function

--- diary.py
+++ diary.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-from bottle import Bottle,route,run,debug
+from bottle import route,run,debug
 
 from datetime import datetime
 import sys
 
-diary=Bottle()
 
-@diary.route('/')
-@diary.route('/hello')
+@route('/')
+@route('/diary')
+def diary():
+    return "哥哥，我是你的日记。"
+
+@route('/hello')
 def hello():
     return "Hello World!"
 
-run(diary,host='localhost', port=1234, debug=True)
+run(host='localhost', port=1234, debug=True)

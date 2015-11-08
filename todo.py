ab1c57c4c9469d4f385ecc83c04279ca81385f3f
Zoe <dadac123@gmail.com>
2015-11-08 11:21:03 +0800

    add template

--- todo.py
+++ todo.py
@@ -7,7 +7,9 @@ def todo_list():
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
-    return str(result)
+    c.close()
+    output = template('make_table', rows=result)
+    return output
 
 @route('/hello')
 def hello():

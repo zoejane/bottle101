142ac07ded0e03d4a7da8831e9eba2726af9d503
Zoe <dadac123@gmail.com>
2015-11-08 11:36:12 +0800

    edit task

--- todo.py
+++ todo.py
@@ -30,7 +30,45 @@ def new_item():
     else:
         return template('new_task.tpl')
 
+@route('/edit/<no:int>', method='GET')
+def edit_item(no):
 
+    if request.GET.get('save','').strip():
+        edit = request.GET.get('task','').strip()
+        status = request.GET.get('status','').strip()
+
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        conn.commit()
+
+        return '<p>The item number %s was successfully updated</p>' % no
+    else:
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
+        cur_data = c.fetchone()
+
+        return template('edit_task', old=cur_data, no=no)
+
+@route('/item<item:re:[0-9]+>')
+def show_item(item):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT task FROM todo WHERE id LIKE ?", (item))
+    result = c.fetchall()
+    c.close()
+    if not result:
+        return 'This item number does not exist!'
+    else:
+        return 'Task: %s' %result[0]
+
+    
 @route('/hello')
 def hello():
     return 'hello'

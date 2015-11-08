23935dce84c5f5738e960cff3d748a542fbc43aa
Zoe <dadac123@gmail.com>
2015-11-08 10:25:35 +0800

    fileupload

--- hello-simple.py
+++ hello-simple.py
@@ -1,4 +1,5 @@
-from bottle import route, run, template
+from bottle import route, run, template,request
+import os
 
 @route('/hello')
 def hello():
@@ -27,5 +28,31 @@ def do_login():
         return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
+
+def check_login(username, password):
+    return True
+
+@route('/upload')
+def upload():
+    return '''
+        <form action="/upload" method="post" enctype="multipart/form-data">
+  Category:      <input type="text" name="category" />
+  Select a file: <input type="file" name="upload" />
+  <input type="submit" value="Start upload" />
+</form>
+    '''
+
+@route('/upload', method='POST')
+def do_upload():
+    category   = request.forms.get('category')
+    upload     = request.files.get('upload')
+    name, ext = os.path.splitext(upload.filename)
+    if ext not in ('.png','.jpg','.jpeg'):
+        return 'File extension not allowed.'
+
+    save_path = get_save_path_for_category(category)
+    upload.save(save_path) # appends upload.filename automatically
+    return 'OK'
+
 
 run(host='localhost', port=8080, debug=True)

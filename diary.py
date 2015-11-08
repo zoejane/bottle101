4c87bd9cdc457ed512d093e1b63f46e608ed3bd8
Zoe <dadac123@gmail.com>
2015-11-08 12:22:27 +0800

    add new diary

--- diary.py
+++ diary.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from bottle import Bottle,route,run,debug
+from bottle import Bottle,route,run,debug,request,template
 
 from datetime import datetime
 import sys
@@ -15,6 +15,32 @@ def greeting():
 <a href='/writing'>写日记<a>
 <a href='/reading'>读日记<a>
 '''
+
+@diary.route('/reading')
+def reading():
+    diaryFile = open('diary.txt')
+    diary = diaryFile.read()
+    return diary
+
+@diary.route('/writing',method='GET')
+def writing():
+    if request.GET.get('save','').strip():
+        '''        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()'''
+
+        return '''
+<p>The new diary was saved.</p>
+<a href='/'>Back Home<a>
+'''
+    else:
+        return template('new_diary.tpl')
 
 @diary.route('/hello')
 def hello():
